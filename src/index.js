86664729eb92df8307e1d5947f3bd84d0da31101
Brian Doherty <bdoherty92@gmail.com>
2017-03-01 19:02:57 -0600

    Remove unused reference to scrollarea

--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ import './css/general/reset.css';
 import './css/general/animate.css';
 import './css/pure/pure-min.css';
 
-import ScrollArea from 'react-scrollbar';
-
 ReactDOM.render(
 	<Window />,
   document.getElementById('root')
